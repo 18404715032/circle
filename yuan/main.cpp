d861c2d335c4d9cb8c712cc0e4f4d22e69ac8083
18404715032 <20141105066yh@admindeiMac-36.local>
2015-12-09 19:05:16 +0800

    将前俩次整合
    将前两次整合后就可以输出周长和面积

--- yuan/main.cpp
+++ yuan/main.cpp
@@ -9,17 +9,26 @@
 #include <iostream>
 using namespace std;
 const float PI=3.1415926;
+float fCir_L(float);
 float fCir_S(float);
 
 int main(int argc, const char * argv[])
 {
-    float r,s;
+    float r,l,s;
     std::cout<<"r=";
     std::cin>>r;
+    l=fCir_L(r);
     s=fCir_S(r);
+    std::cout<<"l="<<l;
     std::cout<<"\ns="<<s;
 }
-
+float fCir_L(float x)
+{
+    float z=0.0;
+    if(x>=0.0)
+        z=2*PI*x;
+    return (z);
+}
 float fCir_S(float x)
 {
     float z=-1.0;
@@ -27,4 +36,3 @@ float fCir_S(float x)
         z=PI*x*x;
     return (z);
 }
-
