4bf6cf85e94a8ab6c6a59740f61ffa5cc3a7fe4e
18404715032 <20141105066yh@admindeimac-36.local>
2015-12-09 18:18:43 +0800

    Initial Commit

---
+++ yuan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  yuan
+//
+//  Created by 杨浩 on 15/12/9.
+//  Copyright © 2015年 杨浩. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
